36f4317878e85e939698791f05d1b7340bd1810b
gnehs <jayuiop@gmail.com>
2019-08-06 21:49:18 +0800

    書庫版面樣式優化

--- components/parseBooks.vue
+++ components/parseBooks.vue
@@ -14,16 +14,21 @@
 		</v-layout>
 		<br />
 		<v-layout v-if="view==='card'" wrap>
-			<v-flex v-for="item in books" :key="item.uuid" xs6 md3>
+			<v-flex v-for="item in books" :key="item.uuid" xs12 sm6 md3>
 				<v-item>
 					<v-card>
 						<v-img class="white--text" height="500px" :src="$store.state.baseurl+item.cover_id" />
 						<v-card-title>{{item.title}}</v-card-title>
-						<v-card-text>{{item.desc.length>150?(item.desc.substr(0,150)+'...'):item.desc}}</v-card-text>
-						<v-card-actions>
+						<v-card-text>{{item.desc&&item.desc.length>150?(item.desc.substr(0,150)+'...'):item.desc}}</v-card-text>
+						<v-card-actions class="hidden-sm-and-down">
 							<v-btn color="primary" text outlined @click="info(item)">詳細資料</v-btn>
 							<v-spacer />
 							<v-btn color="primary" @click="send(item)">發送到 Kindle</v-btn>
+						</v-card-actions>
+						<v-card-actions class="hidden-md-and-up">
+							<v-btn color="primary" text outlined @click="info(item)">詳細</v-btn>
+							<v-spacer />
+							<v-btn color="primary" @click="send(item)">發送</v-btn>
 						</v-card-actions>
 					</v-card>
 				</v-item>
@@ -31,50 +36,60 @@
 		</v-layout>
 		<v-data-table :headers="headers" :items="books" sort-by="name" multi-sort v-if="view==='list'">
 			<template v-slot:item.desc="{ item }">
-				<p>{{item.desc.length>60?(item.desc.substr(0,60)+'...'):item.desc}}</p>
+				<p>{{item.desc&&item.desc.length>60?(item.desc.substr(0,60)+'...'):item.desc}}</p>
 			</template>
 			<template v-slot:item.actions="{ item }">
 				<v-icon small @click="info(item)">mdi-information</v-icon>
 				<v-icon small @click="send(item)">mdi-send</v-icon>
 			</template>
 		</v-data-table>
-		<v-dialog v-model="bookDialog" max-width="500px">
+		<v-dialog v-model="bookDialog" fullscreen transition="dialog-bottom-transition">
 			<v-card>
-				<v-card-title>
-					<span class="headline">詳細資料</span>
-				</v-card-title>
+				<v-toolbar dark color="primary">
+					<v-btn icon dark @click="close">
+						<v-icon>mdi-close</v-icon>
+					</v-btn>
+					<v-toolbar-title>詳細資料</v-toolbar-title>
+					<v-spacer></v-spacer>
+					<v-toolbar-items>
+						<v-btn dark text @click="save">Save</v-btn>
+					</v-toolbar-items>
+				</v-toolbar>
 
 				<v-card-text>
-					<v-container grid-list-sm>
+					<v-container grid-list-md>
 						<v-layout wrap>
-							<v-flex xs12 sm6 md6>
+							<v-flex xs12 sm6 md6 xl4>
 								<v-text-field outlined v-model="editedBook.title" label="標題" />
 							</v-flex>
-							<v-flex xs12 sm6 md6>
+							<v-flex xs12 sm6 md6 xl4>
 								<v-autocomplete label="系列" :items="series" outlined v-model="editedBook.series_id" />
 							</v-flex>
-							<v-flex xs12 sm12 md12>
-								<v-text-field outlined v-model="editedBook.cover_id" readonly label="cover_id" />
+							<v-flex xs12 sm12 md6 xl4>
+								<v-text-field outlined v-model="editedBook.cover_id" readonly label="封面路徑" />
 							</v-flex>
-							<v-layout justify-space-between row>
-								<v-text-field outlined v-model="editedBook.filepath" readonly label="filepath" />
-								<v-btn color="blue darken-1" icon @click="download">
-									<v-icon>mdi-download</v-icon>
-								</v-btn>
-							</v-layout>
+							<v-flex xs12 sm12 md6 xl4>
+								<v-text-field
+									outlined
+									v-model="editedBook.filepath"
+									readonly
+									label="檔案路徑"
+									append-outer-icon="mdi-download"
+								>
+									<template v-slot:append-outer>
+										<v-icon @click="download">mdi-download</v-icon>
+									</template>
+								</v-text-field>
+							</v-flex>
 							<v-flex>
 								<v-textarea outlined v-model="editedBook.desc" label="說明" />
 							</v-flex>
 						</v-layout>
+						<v-card-actions>
+							<v-btn color="blue darken-1" text @click="del">Delete</v-btn>
+						</v-card-actions>
 					</v-container>
 				</v-card-text>
-
-				<v-card-actions>
-					<v-btn color="blue darken-1" text @click="del">Delete</v-btn>
-					<v-spacer></v-spacer>
-					<v-btn color="blue darken-1" text @click="close">Cancel</v-btn>
-					<v-btn color="blue darken-1" text @click="save">Save</v-btn>
-				</v-card-actions>
 			</v-card>
 		</v-dialog>
 		<v-snackbar v-model="sent" :timeout="1500">已發送</v-snackbar>
